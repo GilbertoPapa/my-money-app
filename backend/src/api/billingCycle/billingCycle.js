7565bdc149ec85890e3022e0ed9ce0f18e40ca87
Gilberto Papa <Gilbertopapa013@hotmail.com>
2018-10-30 20:29:55 -0300

    Configurando o Banco de dados

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -1,7 +1,28 @@
-const restful = require('node-restful')
-const mongoose = restful.mongoose
+const restful = require("node-restful");
+const mongoose = restful.mongoose;
 
-const creditSchema = new mongoose.Schema ({
-    name: {type: String, required: true},
-    value: {type: Number, min:0 , require:true}
-})
+const creditSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Number, min: 0, require: true }
+});
+
+const debtSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Number, min: 0, require: true },
+  status: {
+    type: String,
+    required: true,
+    uppercase: true,
+    enum: ["Pago", "Pendente", "Agendado"]
+  }
+});
+
+const billingCycleSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  month: { type: Number, min: 1, max: 12, require: true },
+  year: { type: Number, min: 1970, max: 2100, required: true },
+  credits: [creditSchema],
+  debts: [debtSchema]
+});
+
+module.exports = restful.model("BillingCycle", billingCycleSchema);
