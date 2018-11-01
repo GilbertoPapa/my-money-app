2e34767bf589496c80175880e99789a82ce8bb2a
Gilberto Papa <Gilbertopapa013@hotmail.com>
2018-11-01 18:41:59 -0300

    Mapeamento de Rotas

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+
+mongoose.Error.messages.general.require = "O atributo '{PATH}' é obrigatorio"
